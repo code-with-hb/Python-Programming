f0ca5b3992893ab402194036137944eeb7c81b42
CodeWithHb <123hemantaboro@gmail.com>
2026-05-13 15:07:46 +0530

    Order of operation

--- 6.py
+++ 6.py
@@ -7,4 +7,8 @@ print(a+b)
 print(a-b)
 print(a*b)
 print(a/b)
+#python uses two multiplication symbol to represent exponent.
 print(b**b)
+#python support order of operations too.
+print(a+a*b)
+print((a + b)*b)
