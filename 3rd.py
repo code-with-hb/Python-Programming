559ba817e038db56b88939ef9d1c2d6e35a5ccbf
CodeWithHb <123hemantaboro@gmail.com>
2026-05-13 13:59:42 +0530

    uppercase or lowercase added

--- 3rd.py
+++ 3rd.py
@@ -1,3 +1,7 @@
 #Changing case in a string with methods
 name = "hemanta boro"
 print(name.title())
+#We can change a string to all uppercase or lowercase also
+name = "Hemanta Boro"
+print(name.upper())
+print(name.lower())
