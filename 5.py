ac67625e0350372bd45041d20c2b60a5745141b8
CodeWithHb <123hemantaboro@gmail.com>
2026-05-13 14:39:49 +0530

    Both newlines and tabs were added

--- 5.py
+++ 5.py
@@ -4,3 +4,5 @@ print("Hemanta")
 print("Name: Hemanta\nDoB: 17-02-2000\nDist: Biswanath\nCity: Gohpur")
 #to add a tab to text, we use the charecter combination \t:
 print("\tName: Hemanta, DoB: 17-02-2000, Dist: Biswanath, City: Gohpur")
+#we can also combines tabs and newlines in a single strings
+print("Name: Hemanta\n\tDoB: 17-02-2000\n\t\tDist: Biswanath\n\t\tCity: Gohpur")
