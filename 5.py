c2bb8badae24b26de4d0559bf84828bd0a855310
CodeWithHb <123hemantaboro@gmail.com>
2026-05-13 14:37:32 +0530

    Adding tabs to a text or strings

--- 5.py
+++ 5.py
@@ -2,3 +2,5 @@
 print("Hemanta")
 #to add a newline in a string, use the charecter combination \n:
 print("Name: Hemanta\nDoB: 17-02-2000\nDist: Biswanath\nCity: Gohpur")
+#to add a tab to text, we use the charecter combination \t:
+print("\tName: Hemanta, DoB: 17-02-2000, Dist: Biswanath, City: Gohpur")
