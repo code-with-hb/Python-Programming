2c88047d55778ff50c504bcf7351a58ed7622479
CodeWithHb <123hemantaboro@gmail.com>
2026-05-13 14:16:08 +0530

    we can use f-strings to compose complete message also using the information associated with variable.

--- full_name.py
+++ full_name.py
@@ -4,3 +4,5 @@ last_name = "Boro"
 full_name = f"{first_name} {last_name}" #to insert a variable's value into a string, we place letter f immediately before the opening quotation mark.
 #put braces around any variable to use inside a string.
 print(full_name)
+#we can use f-strings to compose complete message also using the information associated with variable.
+print(f"Hello My name is, {full_name.upper()}")
