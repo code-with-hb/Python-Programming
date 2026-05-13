fa5884cbb55c51a42c1971d19693a7b43f818e90
CodeWithHb <123hemantaboro@gmail.com>
2026-05-13 14:32:43 +0530

    Adding newline to a string

---
+++ 5.py
@@ -0,0 +1,4 @@
+#Adding whitespace to string with tabs or newlines
+print("Hemanta")
+#to add a newline in a string, use the charecter combination \n:
+print("Name: Hemanta\nDoB: 17-02-2000\nDist: Biswanath\nCity: Gohpur")
