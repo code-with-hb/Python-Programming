eaf723efcf8ab60af3f1f4fbca59ae815e6b7e1f
CodeWithHb <123hemantaboro@gmail.com>
2026-05-13 14:09:04 +0530

    uses variable in a string

---
+++ full_name.py
@@ -0,0 +1,6 @@
+#Using variable in a string
+first_name = "Hemanta"
+last_name = "Boro"
+full_name = f"{first_name} {last_name}" #to insert a variable's value into a string, we place letter f immediately before the opening quotation mark.
+#put braces around any variable to use inside a string.
+print(full_name)
