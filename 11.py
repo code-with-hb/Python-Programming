ae18f6607fb4d2161bcb5181f4f78851601ca33d
CodeWithHb <123hemantaboro@gmail.com>
2026-05-15 21:48:25 +0530

    Using Individual values from a list

---
+++ 11.py
@@ -0,0 +1,5 @@
+#USING INDIVIDUAL VALUES FROM A LIST
+#we can use f-strings to create a message based on a value from a list.
+flower =  ['sunflower', ' lotus', 'Rose', 'Hibiscus']
+message  = f"I love {flower[2].upper()}"
+print(message)
