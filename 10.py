92aa1a9f10d557b61feedf22e11982f17cfdab2c
CodeWithHb <123hemantaboro@gmail.com>
2026-05-15 21:41:17 +0530

    Accessing elements in a list

---
+++ 10.py
@@ -0,0 +1,4 @@
+#Accessing Elements in a List
+#list are ordered collections, so we can access any element in a list by telling the Python the position, or index, of the item desired.
+flower = ['sunflower', ' lotus', 'Rose', 'Hibiscus']
+print(flower[1])
