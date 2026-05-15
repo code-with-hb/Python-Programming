a136db7e486413e6d6187dd8828ccc1aacffae7c
CodeWithHb <123hemantaboro@gmail.com>
2026-05-15 21:36:30 +0530

    A simple list

---
+++ 9.py
@@ -0,0 +1,5 @@
+#LIST
+#list is a collection of items in a particular order.For example the letter of Alphabets and number from 0 to 9.
+#In python, Square brackets ([]) indicate a list.
+flower = ['sunflower', 'rose', 'lotus']
+print(flower)
