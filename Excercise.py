2ad6789a4db89c1ef6e439a8f1451c08f69670a0
CodeWithHb <123hemantaboro@gmail.com>
2026-05-16 12:06:00 +0530

    Excercise 1

---
+++ Excercise.py
@@ -0,0 +1,5 @@
+#1. Make a list that includes at least three people you would like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+invitees = ['rabi', 'sagar', 'appa']
+print(f"Dear {invitees[0].title()} you are invited to dinner.")
+print(f"Dear {invitees[1].title()} you are invited to dinner.")
+print(f"Dear {invitees[2].title()} you are invited to dinner.")                 
