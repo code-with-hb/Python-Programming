96d45005f14215067ed9b508c6a9c6d0eb930fa6
CodeWithHb <123hemantaboro@gmail.com>
2026-05-16 12:14:41 +0530

    Excercise 4

--- Excercise.py
+++ Excercise.py
@@ -30,4 +30,19 @@ print(f"Dear {invitees[2].title()} you are invited to dinner.")
 print(f"Dear {invitees[3].title()} you are invited to dinner.")
 print(f"Dear {invitees[4].title()} you are invited to dinner.")     
 print(f"Dear {invitees[5].title()} you are invited to dinner.")
-                                
+#4. You just found out that your new dinner table won't arrive in time for the dinner, and you have space for only two guests. Start with your program from Exercise 3. Add a new line that prints a message saying that you can invite only two people for dinner. Use pop() to remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know you're sorry you can't invite them to dinner. Print a message to each of the two people still on your list, letting them know they're still invited. Use del to remove the last two names from your list, so you have an empty list. Print your list to make sure it's empty.
+invitees = ['rabi', 'suman', 'sushil', 'sagar', 'appa', 'sujan']
+print(f"Sorry, i can invites two people for dinner.")
+popped_guest = invitees.pop(0)
+print(f"Sorry {popped_guest.title()}, you can't be invited to dinner.")
+popped_guest = invitees.pop(1)
+print(f"Sorry {popped_guest.title()}, you can't be invited to dinner.")
+popped_guest = invitees.pop(2)
+print(f"Sorry {popped_guest.title()}, you can't be invited to dinner.")
+popped_guest = invitees.pop(2)
+print(f"Sorry {popped_guest.title()}, you can't be invited to dinner.")
+print(f"\nDear {invitees[0].title()} you are still invited to dinner     .")
+print(f"Dear {invitees[1].title()} you are still invited to dinner.")
+del invitees[0]
+del invitees[0] 
+print(invitees)
