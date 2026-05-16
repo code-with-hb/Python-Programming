1341777b97cda1673eb029bdcd780cc4c7146a9a
CodeWithHb <123hemantaboro@gmail.com>
2026-05-16 11:19:40 +0530

    Insert method to add new element

--- 12.py
+++ 12.py
@@ -14,3 +14,11 @@ bike.append('Hero')
 bike.append('honda')
 bike.append('Yamaha')
 print(bike)
+
+#Inerting Elements into a list.
+#______________________________________________________________
+#we can add a new element at any postion in our list by using the insert() method.
+#We do this by specifying the index of the new element and the value of the new item.
+bike = ['hero', ' yamaha', 'Suzuki']
+bike.insert(0, 'Ducati')
+print(bike)
