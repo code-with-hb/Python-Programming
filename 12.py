8663603ef165a5ffd954881d50da90e0c2ae5140
CodeWithHb <123hemantaboro@gmail.com>
2026-05-16 12:01:53 +0530

    Remove method to work with a value that's being removed

--- 12.py
+++ 12.py
@@ -19,29 +19,36 @@ print(bike)
 #______________________________________________________________
 #we can add a new element at any postion in our list by using the insert() method.
 #We do this by specifying the index of the new element and the value of the new item.
-bike = ['hero', ' yamaha', 'Suzuki']
+bike = ['hero', 'yamaha', 'Suzuki']
 bike.insert(0, 'Ducati')
 print(bike)
 
 #Removing Elements from a list
 #______________________________________________________________
 #If we know the position of the item we want to remove from a list, we can use the del statement.
-bike = ['hero', ' yamaha', 'Suzuki']
+bike = ['hero', 'yamaha', 'Suzuki']
 del bike[1]
 print(bike)
 #Removing items using the pop() Method
-bike = ['hero', ' yamaha', 'Suzuki']
+bike = ['hero', 'yamaha', 'Suzuki']
 popped_bike = bike.pop()
 print(bike)
 print(popped_bike)
 
 #Popping item from any position in a list 
-bike = ['hero', ' yamaha', 'Suzuki']
+bike = ['hero', 'yamaha', 'Suzuki']
 last_owened = bike.pop(1)
 print(f"My last owned bike was a {last_owened.title()}")
 
 #Removing an item by value
 #If we know the value of the item we want to remove, we can use the remove() Method.
-bike = ['hero', ' yamaha', 'Suzuki']
+bike = ['hero', 'yamaha', 'Suzuki']
 bike.remove('hero')
 print(bike)
+#We can also use the remove method to work with a value that's being removed from a list.
+bike = ['hero', 'yamaha', 'Suzuki']
+print(bike)
+too_expensive = 'yamaha'
+bike.remove(too_expensive)
+print(bike)
+print(f"\nA {too_expensive.title()} is too expensive for me.")  
