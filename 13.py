e6046870733f46e1b76d4c7bfd584e67b0d0e6ae
CodeWithHb <123hemantaboro@gmail.com>
2026-05-17 20:47:59 +0530

    Sort method to list alphabetically

---
+++ 13.py
@@ -0,0 +1,7 @@
+#ORGANIZING A LIST
+#1. Soriting a list permanently with sort method
+#python sort method makes it relatively easy to sort a list.
+#it helps to orgainize a list in alphabetical oreder.
+cars = ['bmw','audi','tata','mahindra']
+cars.sort()
+print(cars)
